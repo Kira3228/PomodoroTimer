5c94e08baf399e20ac2f5e110d7fc6460d8767a1
Kira3228 <hanzomein228@gmail.com>
2024-07-03 01:20:34 +0300

    first commit

---
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.pomodorotimer;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_POST_NOTIFICATIONS = 1;
+    private TextView timerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        Button startButton = findViewById(R.id.startButton);
+        Button stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkNotificationPermissionAndStartService();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService();
+
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkNotificationPermission();
+        }
+    }
+
+    private void checkNotificationPermissionAndStartService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.POST_NOTIFICATIONS},
+                        REQUEST_CODE_POST_NOTIFICATIONS);
+            } else {
+                startService();
+            }
+        } else {
+            startService();
+        }
+    }
+
+    private void startService() {
+        if (!isMyServiceRunning(PomodoroTimerService.class)) {
+            Intent serviceIntent = new Intent(this, PomodoroTimerService.class);
+            serviceIntent.putExtra("inputExtra", "Pomodoro Timer is running");
+            ContextCompat.startForegroundService(this, serviceIntent);
+        }
+    }
+
+    private void stopService() {
+        Intent serviceIntent = new Intent(this, PomodoroTimerService.class);
+        stopService(serviceIntent);
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.POST_NOTIFICATIONS},
+                    REQUEST_CODE_POST_NOTIFICATIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_POST_NOTIFICATIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startService();
+            } else {
+                // Разрешение не предоставлено. Обработайте отказ.
+                handlePermissionDenied();
+            }
+        }
+    }
+
+    private void handlePermissionDenied() {
+        Snackbar.make(findViewById(android.R.id.content), "Разрешение на уведомления необходимо для работы таймера.", Snackbar.LENGTH_LONG)
+                .setAction("Настройки", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Открытие настроек приложения для включения разрешения
+                        Intent intent = new Intent();
+                        intent.setAction(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        intent.addCategory(Intent.CATEGORY_DEFAULT);
+                        intent.setData(Uri.parse("package:" + getPackageName()));
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                        startActivity(intent);
+                    }
+                }).show();
+
+    }
+
+    public void updateTimer(long millisUntilFinished) {
+        int minutes = (int) (millisUntilFinished / 1000) / 60;
+        int seconds = (int) (millisUntilFinished / 1000) % 60;
+        String timeLeftFormatted = String.format("%02d:%02d", minutes, seconds);
+        timerTextView.setText(timeLeftFormatted);
+    }
+
+}
+
---
+++ app/src/main/java/com/example/pomodorotimer/PomodoroTimerService.java
@@ -0,0 +1,141 @@
+package com.example.pomodorotimer;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.os.Vibrator;
+
+import androidx.core.app.NotificationCompat;
+
+public class PomodoroTimerService extends Service {
+    public static final String CHANNEL_ID = "PomodoroTimerChannel";
+    private CountDownTimer countDownTimer;
+    //private long timeLeftInMillis = 1500000; // 25 minutes
+    private long timeLeftInMillis;
+    private boolean isTimerRunning = false;
+    private int sessionCount = 0;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Pomodoro Timer")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent);
+
+        startForeground(1, notification.build());
+        startTimer();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateNotification();
+            }
+            @Override
+            public void onFinish() {
+                sessionCount++;
+                if (sessionCount % 4 == 0) {
+                    timeLeftInMillis = 15 * 60 * 1000; // 15 minutes break
+                    //timeLeftInMillis = 15000;
+                    //vibratePhone();
+                } else if (sessionCount % 2 == 0) {
+                    timeLeftInMillis = 5 * 60 * 1000; // 5 minutes break
+                    //timeLeftInMillis = 10000;
+                    //vibratePhone();
+                } else {
+                    timeLeftInMillis = 25 * 60 * 1000; // 25 minutes work
+                    //vibratePhone();
+                }
+                sendNotification("Pomodoro Timer123123123", "Time to switch!");
+                //vibratePhone();
+                startTimer();
+            }
+        }.start();
+        isTimerRunning = true;
+    }
+
+    private void sendNotification(String title, String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                //.setVibrate(new long[]{0, 0, 0, 0, 0})
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000});
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(2, builder.build());
+        vibratePhone();
+    }
+
+    private void vibratePhone() {
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(500);
+    }
+
+    private NotificationCompat.Builder notificationBuilder;
+
+    private void updateNotification() {
+        if (notificationBuilder == null){
+            Intent notificationIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                    0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
+            notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("Pomodoro Timer")
+                    .setContentText("Time remaining: " + timeLeftInMillis / 1000)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentIntent(pendingIntent)
+                    .setVibrate(null) // Устанавливаем null для отключения вибрации
+                    .setSound(null); // Устанавливаем null для отключения звука уведомления
+
+            startForeground(1, notificationBuilder.build());
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Pomodoro Timer Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+}
+
